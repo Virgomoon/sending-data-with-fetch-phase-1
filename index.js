d2780539cff2ce26f5c78b7f04094edd2c471af2
Joseph Guillory <jagmanx99@yahoo.com>
2022-04-30 21:47:16 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        })
+    })
+    .then(function (response) {
+        return response.json();
+     })
+     .then(function(object){
+         const p = document.createElement('p')
+         document.querySelector('body').appendChild(p)
+         p.innerHTML = object.id
+     })
+     .catch(function(error){
+        const p = document.createElement('p')
+        document.querySelector('body').appendChild(p)
+         p.innerHTML = error.message
+     })
+}
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+  
